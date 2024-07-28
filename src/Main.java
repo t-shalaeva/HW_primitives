16ccf5db26beaa7788a1fcdbdc8121e09f858bba
Shalaeva T <alice.contist@gmail.com>
2024-07-28 20:05:39 +0300

    Add HW

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        int ticketCost = 107895;
+        int oneMileCost = 20;
+        int countMile = ticketCost / oneMileCost;
 
+        System.out.println("Количество начисленных миль: " + countMile);
     }
 }
